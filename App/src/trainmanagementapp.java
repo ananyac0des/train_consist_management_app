42b86930cfe5856806b520544bc0b0019750e172
Ananya Pradhan <ananyapradhan06@gmail.com>
2026-04-02 14:16:26 +0530

    UC1

--- App/src/trainmanagementapp.java
+++ App/src/trainmanagementapp.java
@@ -5,7 +5,7 @@ public class trainmanagementapp {
 
     public static void main(String[] args) {
 
-        System.out.println("=== Train Consist Management App ===");
+        System.out.println("Train Consist Management App");
         List<String> trainConsist = new ArrayList<>();
 
         System.out.println("Train consist initialized.");
